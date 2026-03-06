4fe629f4ba379d6d7892feac6574831c071886a5
Matthew Denton <eduardermakov769@gmail.com>
2026-03-06 11:35:34 +0800

    Update setup_dapp.js

--- examples/setup_dapp.js
+++ examples/setup_dapp.js
@@ -1,4 +1,4 @@
-// setup_dapp.js KINHEX
+// setup_dapp.js 
 // Example of setting up a frontend DApp with wallet integration for Solana blockchain
 
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from '@solana/web3.js';
